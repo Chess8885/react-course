709fd3a1ae71c340e3aa54db48e56c247422eb7e
Available web developer <alexandre.ohayon@epitech.eu>
2022-04-01 12:21:02 +0200

    [ADD] clean App.js

--- day12/myapp/src/App.js
+++ day12/myapp/src/App.js
@@ -6,39 +6,8 @@ import Todo from "./Todo";
 
 export default function App() {
     return (
-        <div className="App">
-            <Router>
-                <Switch>
-                    <Route exact path="/" children={<Todos />} />
-                    <Route path="/todo/:id" children={<Todo />} />
-                </Switch>
-            </Router>
-        </div>
-    );
-}
-// You can think of these components as "pages"
-// in your app.
-
-function Home() {
-    return (
         <div>
-            <h2>Home</h2>
-        </div>
-    );
-}
 
-function About() {
-    return (
-        <div>
-            <h2>About</h2>
-        </div>
-    );
-}
-
-function Dashboard() {
-    return (
-        <div>
-            <h2>Dashboard</h2>
         </div>
     );
 }
