387743e3b898834bf9d52dff1c8a6a47ee55b933
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2022-04-04 22:11:10 +0200

    [ADD] day12 beginning

--- day12/myapp/src/App.js
+++ day12/myapp/src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Todos from "./Todos";
 import Todo from "./Todo";
 
 export default function App() {
+    const [count, setCount] = useState(0);
+
+    const addCounter = () => {
+        setCount(count+1);
+    }
     return (
         <div>
-
+        <h2>redux made easy</h2>
+            <p>{count}</p>
+            <button onClick={addCounter}>add count</button>
         </div>
     );
 }
