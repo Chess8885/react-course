394f971c2a19d4a3c4e1c95ccce6a49a836a69c1
Available web developer <alexandre.ohayon@epitech.eu>
2022-04-15 14:56:20 +0200

    [ADD] setup redux ducks

--- day12/myapp/src/App.js
+++ day12/myapp/src/App.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Todos from "./Todos";
 import Todo from "./Todo";
+import store from './redux/configureStore';
 
 export default function App() {
     const [count, setCount] = useState(0);
