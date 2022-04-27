4652285533b370e553c9af10d65c6a6cf21bc813
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2022-04-27 22:34:25 +0200

    [ADD] redux implementation functional day12

--- day12/myapp/src/App.js
+++ day12/myapp/src/App.js
@@ -4,18 +4,33 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Todos from "./Todos";
 import Todo from "./Todo";
 import store from './redux/configureStore';
+import Counter from "./counter";
 
+export default function App() {
+    return (
+        <div>
+        <Counter/>
+        </div>
+    );
+}
+
+{ /*
 export default function App() {
     const [count, setCount] = useState(0);
 
     const addCounter = () => {
         setCount(count+1);
     }
+    const removeCounter = () => {
+        setCount(count-1);
+    }
     return (
         <div>
-        <h2>redux made easy</h2>
+            <h2>redux made easy</h2>
             <p>{count}</p>
             <button onClick={addCounter}>add count</button>
+            <button onClick={removeCounter}>rem count</button>
         </div>
     );
 }
+ */}
